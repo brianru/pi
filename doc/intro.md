9ec3d8eaa160c39d74669c02152fae7d1683f3bc
brianru <brianrubinton@gmail.com>
2014-09-12 14:56:40 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to x
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject x "0.1.0-SNAPSHOT"
+  :description "Hyper Local Information"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
