f294cb73199f5f9525878f69ef99dccade9d89a3
brianru <brianrubinton@gmail.com>
2014-09-13 18:01:45 -0400

    generating a page with om

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url ""}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2322"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [om "0.7.3"]
+                 [prismatic/om-tools "0.3.2"]
                  [environ "1.0.0"]
                  [ring/ring-core "1.3.1"]
                  [javax.servlet/servlet-api "2.5"] ;dev only ?
@@ -16,8 +19,22 @@
   :min-lein-version "2.0.0"
   :main main
   :plugins [[lein-cljsbuild "1.0.3"]]
-  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src-cljs"]
                         :compiler {:output-to "static/main.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
+                                   :output-dir "static/out"
+                                   :optimizations :none
+                                   :pretty-print true
+                                   :source-map true}}
+                       {:id "release"
+                        :source-paths ["src-cljs"]
+                        :compiler {:output-to "static/main.js"
+                                   :optimizations :advanced
+                                   :elide-asserts true
+                                   :pretty-print false
+                                   :output-wrapper false
+                                   :preamble ["react/react.min.js"]
+                                   :externs ["react/externs/react.js"]
+                                   :closure-warnings {:externs-validation :off
+                                                      :non-standard-jsdoc :off}}}]}
   )
--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -1,82 +1,133 @@
 (ns main
-  (:require [geo.core :as geo]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [geo.core :as geo]))
 
-(def i (js/$ "#i"))
-(def history (js/$ "#history"))
-(def location (js/$ "#location"))
+(enable-console-print!)
 
 (defn- now [] 
   (quot (.getTime (js/Date.)) 1000))
 
-(def max-id (atom 0))
-(def cur-loc (atom {:latitude nil :longitude nil}))
+(def app-state (atom {:max-id 0
+                      :cur-loc {:latitude nil
+                                :longitude nil}
+                      :post ""
+                      :username ""
+                      :messages []}))
 
-(defn- set-loc [x]
-  (let [lat js/x.coords.latitude
-        lon js/x.coords.longitude]
-    (swap! cur-loc #(merge % {:latitude lat :longitude lon}))
-    (.text location (str "lat: " lat ", "
-                         "long: " lon))))
 
-(defn- loc [] 
-  (if (.hasOwnProperty js/navigator "geolocation")
-    (.getCurrentPosition js/navigator.geolocation set-loc)))
-(loc)
+(defn display-location [{:keys [latitude longitude]}]
+  (str "lat: " latitude ", long: " longitude))
 
-(defn distance [msg-loc]
-  (let [my-loc (.-state cur-loc)
-        pt1 (geo/point 4326 (:latitude my-loc) (:longitude my-loc))
-        pt2 (geo/point 4326 (.-latitude msg-loc) (.-longitude msg-loc))
-        dist (geo/distance-to pt1 pt2)]
-    (str dist "km")))
+(defn handle-change [e owner {:keys [post]}]
+  (om/set-state! owner :post (.. e -target -value)))
 
-(defn add-msg [msg]
-  (js/console.log msg)
-  (let [t (str "<span class=\"time\">" (- (now) (.-time msg))  "s ago</span>")
-        dist (str "<span class=\"distance\">" (distance (.-location msg)) "</span>")
-        author (str "<span class=\"author\">" (.-author msg) "</span>: ")]
-    (.append history (str "<li>" author (.-msg msg) dist t "</li>"))))
+(defn submit-post [app owner]
+  (let [new-post (-> (om/get-node owner "new-post")
+                     .-value)]
+    (when new-post
+      (om/transact! app :messages #(conj % new-post))
+      (om/set-state! owner :post ""))))
 
-(def conn 
-  (js/WebSocket. (str "ws://" js/document.location.host "/ws")))
+(defn messages-view [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:post ""})
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+        (dom/h2 nil (display-location (:cur-loc app)))
+        (apply dom/ul nil
+               (om/build-all message-view (:messages app)
+                             {:init-state state}))
+        (dom/div nil
+          (dom/input #js {:type "text" :ref "new-post"
+                          :value (:post state)
+                          :onChange #(handle-change % owner state)})
+          (dom/button #js {:onClick #(submit-post app owner)}
+                      "Submit"))))))
 
-(set! (.-onopen conn)
-  (fn [e]
-    (.send conn
-      (.stringify js/JSON (js-obj "command" "getall")))))
+(om/root
+  messages-view app-state
+  {:target (. js/document (getElementById "messages"))})
 
-(set! (.-onerror conn) 
-  (fn []
-    (js/alert "error")
-    (.log js/console js/arguments)))
+;(def i (js/$ "#i"))
+;(def history (js/$ "#history"))
+;(def location (js/$ "#location"))
 
-(set! (.-onmessage conn)
-  (fn [e]
-    (let [msgs (.parse js/JSON (.-data e))]
-      (doseq [msg msgs]
-         (if (> (.-id msg) (.-state max-id))
-           (do
-             (add-msg msg)
-             (js/console.log msg)
-             (swap! max-id #(.-id msg))))))))
 
-(defn reset-input []
-  (.val i ""))
+;(defn- set-loc [x]
+;  (let [lat js/x.coords.latitude
+;        lon js/x.coords.longitude]
+;    (swap! cur-loc #(merge % {:latitude lat :longitude lon}))
+;    (.text location (str "lat: " lat ", "
+;                         "long: " lon))))
+;
+;(defn- loc [] 
+;  (if (.hasOwnProperty js/navigator "geolocation")
+;    (.getCurrentPosition js/navigator.geolocation set-loc)))
+;(loc)
+;
+;(defn distance
+;  "TODO I don't know if these numbers are correct.
+;   What's the 4326 all about?"
+;  [msg-loc]
+;  (let [my-loc (.-state cur-loc)
+;        pt1 (geo/point 4326 (:latitude my-loc) (:longitude my-loc))
+;        pt2 (geo/point 4326 (.-latitude msg-loc) (.-longitude msg-loc))
+;        dist (geo/distance-to pt1 pt2)]
+;    (str dist "km")))
+;
+;(defn add-msg [msg]
+;  (js/console.log msg)
+;  (let [t (str "<span class=\"time\">" (- (now) (.-time msg))  "s ago</span>")
+;        dist (str "<span class=\"distance\">" (distance (.-location msg)) "</span>")
+;        author (str "<span class=\"author\">" (.-author msg) "</span>: ")]
+;    (.append history (str "<li>" author (.-msg msg) dist t "</li>"))))
+;
+;(def conn 
+;  (js/WebSocket. (str "ws://" js/document.location.host "/ws")))
+;
+;(set! (.-onopen conn)
+;  (fn [e]
+;    (.send conn
+;      (.stringify js/JSON (js-obj "command" "getall")))))
+;
+;(set! (.-onerror conn) 
+;  (fn []
+;    (js/alert "error")
+;    (.log js/console js/arguments)))
+;
+;(set! (.-onmessage conn)
+;  (fn [e]
+;    (let [msgs (.parse js/JSON (.-data e))]
+;      (doseq [msg msgs]
+;        ;; if message is new and it's for me, then...
+;         (if (> (.-id msg) (.-state max-id))
+;           (do
+;             (add-msg msg)
+;             (swap! max-id #(.-id msg))))))))
+;
+;(defn reset-input []
+;  (.val i ""))
+;
+;(defn send-to-server []
+;  (let [msg (.trim js/$ (.val i))
+;        author (.trim js/$ (.val (js/$ "#name")))
+;        payload (js-obj "msg" msg
+;                        "author" author
+;                        "location" (clj->js (.-state cur-loc)))]
+;    (if msg
+;      (do
+;        (.send conn (.stringify js/JSON payload))
+;        (reset-input)
+;        ))))
 
-(defn send-to-server []
-  (let [msg (.trim js/$ (.val i))
-        author (.trim js/$ (.val (js/$ "#name")))
-        payload (js-obj "msg" msg
-                        "author" author
-                        "location" (clj->js (.-state cur-loc)))]
-    (if msg
-      (do
-        (.send conn (.stringify js/JSON payload))
-        (reset-input)
-        ))))
+;(.click (js/$ "#send") send-to-server)
 
-(.click (js/$ "#send") send-to-server)
-
-(.keyup (.focus i) 
-  (fn [e]
-    (if (= (.-which e) 13) (send-to-server))))
+;(.keyup (.focus i) 
+;  (fn [e]
+;    (if (= (.-which e) 13) (send-to-server))))
