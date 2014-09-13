96aa637bd7db25e9d41392a8ee0dac6e0b533549
brianru <brianrubinton@gmail.com>
2014-09-13 08:45:19 -0400

    heroku deploy not working

--- src/main.clj
+++ src/main.clj
@@ -73,5 +73,5 @@
 ;      resp)))
 
 (defn -main [& args]
-  (run-server (-> #'chatrootm site) {:port 9899}))
+  (run-server (-> #'chatrootm site) {:port 80}))
   ;(info "server started. http://127.0.0.1:9899"))
