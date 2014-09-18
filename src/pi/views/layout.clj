f47e782e6efa331834410e7f42d26ff853b19827
brianru <brianrubinton@gmail.com>
2014-09-18 09:26:35 -0400

    uberjar works, send react over https when applicable

--- src/pi/views/layout.clj
+++ src/pi/views/layout.clj
@@ -25,7 +25,7 @@
     (head)
     [:body
      [:div#app-container
-      (include-js "http://fb.me/react-0.11.1.js"
+      (include-js "//fb.me/react-0.11.1.js"
                   "//cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.3/moment.min.js"
                   "js/out/goog/base.js"
                   "js/main.js")
