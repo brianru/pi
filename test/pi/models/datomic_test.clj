9cce496272e4656918418507a6f9b193a48cb5ee
brianru <brianrubinton@gmail.com>
2014-10-10 11:54:57 -0400

    stub datomic component tests

---
+++ test/pi/models/datomic_test.clj
@@ -0,0 +1,26 @@
+(ns pi.models.datomic-test
+  (:require [clojure.test :refer :all]
+            [com.stuartsierra.component :as component]
+            [pi.models.datomic :refer :all]))
+
+(defn- fresh-db []
+  (database "localhost" 4334))
+
+(deftest start
+  (is ((complement nil?) (-> (fresh-db)
+                             component/start
+                             :connection))))
+
+(deftest stop
+  (is (nil? (-> (fresh-db)
+                component/start
+                component/stop
+                :connection))))
+
+(test-ns *ns*)
+
+;(deftest query)
+
+;(deftest transact)
+
+;; (deftest schema)
